1f3b23224ce1d4fe623c6307002cd9dd5cf10878
Peter Miklos <pmiklos@sirokko.net>
2023-06-08 00:35:38 -0700

    added more FAQ questions and answers

--- src/jsMain/kotlin/faq/view/FaqView.kt
+++ src/jsMain/kotlin/faq/view/FaqView.kt
@@ -15,8 +15,36 @@ fun Faq() {
         item(title = "What is Crypto Funds?", expand = true) {
             Text(
                 """Crypto Funds is a DeFi protocol that allows users to create and manage a basket of crypto assets. 
-                |Each fund issues shares that represent the underlying assets. 
+                |Users can receive shares directly from the Fund by locking up the managed assets in the requests ratio.
+                |Similarly, users can redeem the underlying assets by retuning the shares.""".trimMargin()
+            )
+        }
+        item(title = "Who manages the Crypto Funds?") {
+            Text(
+                """Crypto Funds are managed by the community. There is no central entity that manages the funds.
+                |Anyone can deploy a fund on the distributed ledger and anyone can send assets to it to receive shares
+                |or return shares to redeem assets.""".trimMargin()
+            )
+        }
+        item(title = "What are Fund Shares?") {
+            Text(
+                """Fund shares are tokens that represent the underlying assets held by the fund.
+                |For example, if a fund holds 1 BTC and 10 ETH and the there are 100 shares issued,
+                |then each share of the fund represents 0.01 BTC and 0.1 ETH. 
+                |Shares are issued proportionate to the assets sent to the fund, 
+                |and shares can be returned to the fund to redeem the underlying assets in the same proportion.
+                |Using the previous example, if a user sends 1 share to the fund, then they will receive 0.01 BTC and 0.1 ETH.
                 |The fund shares can be traded on decentralized exchanges (DEXes) and stored in crypto wallets.""".trimMargin()
+            )
+        }
+        item(title = "How can I buy Fund Shares?") {
+            Text(
+                """Fund shares can be bought either directly from the fund or on secondary exchanges. 
+                |When buying shares directly from the fund, the payment is made in the underlying assets
+                |in the required proportion defined by the fund. 
+                |When purchasing shares on secondary markets, the payment is made in the quote currency. 
+                |For example, if an exchange lists the Fund Share in USDC coins, one can buy shares in USDC.
+                |""".trimMargin()
             )
         }
         item(title = "What blockchain does Crypto Funds use?") {
